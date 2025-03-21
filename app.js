b25a2e69e40421b41916464570e0e7b755fc8623
Ajay Nishad <nishadajay1099@gmail.com>
2025-03-21 09:17:19 +0530

    wrote the logic for RBG to HEX and HSL

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
+
 var ejs = require('ejs');
+
+var convert = require('color-convert');
 const port = 3001
 
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render("index",{convert:convert})
 })
 
 app.listen(port, () => {
